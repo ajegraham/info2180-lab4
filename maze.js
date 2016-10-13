c7197fca70ca599af14ba297bb0b8b30eff66ea2
ajegraham <jakiepooh23@gmail.com>
2016-10-13 12:23:36 -0500

    single boundary turns red

---
+++ maze.js
@@ -0,0 +1,8 @@
+window.onload= function(){
+
+	var x = document.getElementById("boundary1");
+	x.addEventListener("mouseover", function(){
+		this.setAttribute("class", "boundary youlose");	
+		});
+
+};
