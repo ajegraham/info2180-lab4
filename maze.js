0f22551d7c2f7f6742b01d994b4433428c0cc92d
ajegraham <jakiepooh23@gmail.com>
2016-10-14 20:37:35 -0500

    restartable maze

--- maze.js
+++ maze.js
@@ -4,6 +4,7 @@ window.onload= function(){
 	var e = document.getElementById("end");
 	var boundary = false;
 	var status = document.getElementById("status");
+	var s = document.getElementById("start");
 	
 
 		
@@ -14,6 +15,26 @@ window.onload= function(){
 			}
 		boundary=true;
 		}
+	}
+	
+	s.onclick = function(){
+    for (var i=0; i<y.length; i++){
+      y[i].onmouseover = function()
+      {
+        for (var j=0;j<y.length-1; j++){
+          y[j].className = "boundary youlose";
+          status.innerHTML = "You Lose!";
+        }
+        boundary=true;
+      }
+    }
+
+    for (var i=0;i<y.length-1; i++){
+      y[i].className ="boundary";
+    }
+    status.innerHTML = "Move your mouse over the \"S\" to begin.";
+    boundary = false;
+	
 	}
 	
 	
