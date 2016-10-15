68c66cb45a6095036e21befb5b53197e46e37f9f
ajegraham <jakiepooh23@gmail.com>
2016-10-14 20:50:37 -0500

    disallow cheating

--- maze.js
+++ maze.js
@@ -5,6 +5,7 @@ window.onload= function(){
 	var boundary = false;
 	var status = document.getElementById("status");
 	var s = document.getElementById("start");
+	var maze = document.getElementById("maze");
 	
 
 		
@@ -18,25 +19,38 @@ window.onload= function(){
 	}
 	
 	s.onclick = function(){
-    for (var i=0; i<y.length; i++){
-      y[i].onmouseover = function()
-      {
-        for (var j=0;j<y.length-1; j++){
-          y[j].className = "boundary youlose";
-          status.innerHTML = "You Lose!";
-        }
-        boundary=true;
-      }
-    }
+		for (var i=0; i<y.length; i++){
+		  y[i].onmouseover = function()
+		  {
+			for (var j=0;j<y.length-1; j++){
+			  y[j].className = "boundary youlose";
+			  status.innerHTML = "You Lose!";
+			}
+			boundary=true;
+		  }
+		}
 
-    for (var i=0;i<y.length-1; i++){
-      y[i].className ="boundary";
-    }
-    status.innerHTML = "Move your mouse over the \"S\" to begin.";
-    boundary = false;
-	
+		for (var i=0;i<y.length-1; i++){
+		  y[i].className ="boundary";
+		}
+		status.innerHTML = "Move your mouse over the \"S\" to begin.";
+		boundary = false;
+		
+		
+		
+		document.onmouseover = function(some){
+			var a = some.clientX;
+			var b = some.clientY;
+
+			if((a<maze.offsetLeft)||(a>maze.offsetRight)||(b<maze.offsetTop)||(b>maze.offsetBottom)){
+				for (var j=0;j<y.length-1; j++){
+					y[j].setAttribute("class","boundary youlose");
+					status.innerHTML = "You Lose!";
+					boundary=true;
+				}
+			}
+		}
 	}
-	
 	
 	e.onmouseover = function(){
       if(boundary){
