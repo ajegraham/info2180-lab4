3794e9596faba13f6034b8ee6efdf24537f7a346
ajegraham <jakiepooh23@gmail.com>
2016-10-14 20:16:05 -0500

    all boundaries glow red

--- maze.js
+++ maze.js
@@ -1,8 +1,17 @@
 window.onload= function(){
-
 	var x = document.getElementById("boundary1");
-	x.addEventListener("mouseover", function(){
-		this.setAttribute("class", "boundary youlose");	
-		});
+	var y = document.querySelectorAll(".boundary");
+	
+
+		
+	for (var i=0; i<y.length; i++){
+		y[i].onmouseover = function(){
+			for (var j=0;j<y.length-1; j++){
+				y[j].className = "boundary youlose";
+			}
+		}
+	}
+	
 
 };
+
